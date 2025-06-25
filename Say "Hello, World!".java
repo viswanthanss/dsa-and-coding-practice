527a7b93719bedb7f1ce992e213f9e3483722bf7
Viswanthan S S <imviswanthanss@gmail.com>
2025-06-25 21:55:52 +0530

    Update Say "Hello, World!".java

--- Say "Hello, World!".java
+++ Say "Hello, World!".java
@@ -4,6 +4,20 @@ public class Main {                           // Main class required by Hackerra
     }
 }
 
-
+/* 
 \" → This tells Java to print the double quote character, not end the string.
 So "\"Hello, World!\"" will output exactly: "Hello, World!"
+*/
+---------------------------------------------------------------------------------
+
+// Hello World with tab:
+
+public class Solution {                           // Required class name for Hackerrank to compile
+    public static void main(String[] args) {      // Entry point of the Java program
+        System.out.println("Hello World\tHello World");  
+        // \t adds a tab space between the two strings
+    }
+}
+
+----------------------------------------------------------------------------------
+
