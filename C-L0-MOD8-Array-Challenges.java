972253dacf5fdb2c5d73846e14737e22992f9b19
Viswanthan S S <imviswanthanss@gmail.com>
2025-06-26 12:26:33 +0530

    Update C-L0-MOD8-Array-Challenges.java

--- C-L0-MOD8-Array-Challenges.java
+++ C-L0-MOD8-Array-Challenges.java
@@ -81,6 +81,58 @@ public class Solution {
 
 ---------------------------------------------------------------------------------------------
 
+import java.util.*;
+import java.util.Scanner;
+import java.math.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);        // Scanner for input
+
+        int n = sc.nextInt();                       // Read number of elements
+        int[] arr = new int[n];                     // Declare array
+
+        int sum = 0;                                // Variable to store the sum
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();                  // Read array element
+            sum += arr[i];   //math                       // Add to sum
+            //sum = sum + arr[i];
+            
+        }
+
+        System.out.println(sum);                    // Print the total sum
+    }
+}
+
+
+-----------------------------------------------------------------------------------------
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);     // Read input
+        int n = sc.nextInt();                    // Number of elements
+        int[] arr = new int[n];                  // Declare array
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();               // Input elements
+        }
+
+        int sum = 0;                              // To store running sum of left elements
+        for (int i = 0; i < n; i++) {
+            System.out.print(sum + " ");         // Print current sum (left side)
+            sum += arr[i];                       // Add current element to sum
+            //sum holds the sum of all previous (left) elements
+           // We want to print that left-side sum before adding the current element
+        }
+    }
+}
+
+
+
+-----------------------------------------------------------------------------------------
 
 
 
