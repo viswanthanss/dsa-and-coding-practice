3a9d2ca05f5847c6017a0fea76eec7e20574dd33
Viswanthan S S <imviswanthanss@gmail.com>
2025-06-27 16:09:33 +0530

    Update Leader Number.java

--- Leader Number.java
+++ Leader Number.java
@@ -1,33 +1,34 @@
 import java.util.*;
 
-public class leader_number
+public class Main
 {
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
         
-        int n = sc.nextInt();           // Input size 
-        int[] a = new int[n];           // arr declare
-        // int i;   //common                    
+        int n = sc.nextInt();           // Input size of array
+        int[] a = new int[n];           // Declare array
+        int i;                          // Loop variable
         
-        for(int i = 0; i < n; i++)          // Read arr elements
+        for(i = 0; i < n; i++)          // Read array elements
         {
-            a[i] = sc.nextInt();  
+            a[i] = sc.nextInt();
         }
         
-        int max = a[n - 1];            //last 
-        System.out.print(max + " ");    // print first leader 
+        int max = a[n - 1];             // Start from last element
+        System.out.print(max + " ");    // Last element is always a leader
         
-        for(int i = n - 2; i >= 0; i--)     // Traverse from right to left
+        for(i = n - 2; i >= 0; i--)     // Traverse from right to left
         {
             if(a[i] > max)              // If current is greater than max
             {
                 max = a[i];             // Update max
-                System.out.print(max + " ");  // Print 
+                System.out.print(max + " ");  // Print leader
             }
         }
     }
 }
 
 
-// https://onlinegdb.com/vHHyCQRYY
+
+// https://chatgpt.com/share/685e74d9-3db8-8010-998a-524c582100cb
