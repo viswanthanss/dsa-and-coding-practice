76860c787d652abb924ebb6412a8950a8286c19b
Viswanthan S S <imviswanthanss@gmail.com>
2025-07-01 12:30:26 +0530

    Create Print Prime Numbers in a range.java

---
+++ Fundamentals of Programming /Print Prime Numbers in a range.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Solution 
+{
+  public static void main(String[] args) 
+  {
+    Scanner sc = new Scanner(System.in);      
+    int n = sc.nextInt();                     
+
+    for (int i = 2; i <= n; i++) 
+    {            
+      int count = 0;                         
+
+      for (int j = 1; j <= i; j++) 
+      {          
+        if (i % j == 0) 
+        {                  
+          // count = count + 1;
+            count++;
+        }
+      }
+
+      if (count == 2) 
+      {                      
+        System.out.print(i + " ");            
+      }
+    }
+  }
+}
