d7b85d645ac22dc236c531c2f0d1c8831d54927c
Viswanthan S S <imviswanthanss@gmail.com>
2025-07-02 21:40:42 +0530

    Create Replace.java

---
+++ MyPrep/Replace.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{
+  public static void main(String[] args) 
+  {
+    Scanner sc = new Scanner(System.in);         
+    String line = sc.nextLine();                 
+    String ch = sc.next();  
+    // String ch ;
+    // ch = "i";
+    System.out.println(line.replace(ch, "")); 
+  }
+}
+
+//frequently asked interview qns
