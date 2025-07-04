78dfdd058f1573df3d5ac58f9a4d711298c3f930
Viswanthan S S <imviswanthanss@gmail.com>
2025-07-04 12:12:19 +0530

    Create C-L0-MOD11-Challenge2.java

---
+++ C-LEVEL0-MOD11-2D/C-L0-MOD11-Challenge2.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+
+public class Solution 
+{
+
+    public static void main(String[] args) 
+    {
+        
+        Scanner sc=new Scanner(System.in);
+        
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        
+        int[][] arr= new int[m][n];    //int[] arr= new int[n];
+        
+        for(int i=0;i<m;i++)     //[1,n]
+        {
+            for(int j=0;j<n;j++)
+            {
+                arr[i][j]=sc.nextInt();    //arr[i] = sc.nextInt();
+            }
+            
+            
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print(arr[i][j]+" ");   //space
+            } 
+            
+            System.out.println();    //new line //inside the loop
+        } 
+        
+        
+    }
+}
